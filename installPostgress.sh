192a653a3f3bb24fdf767eeca7b6c2f52cf7f782
Roman <babkamen@gmail.com>
2016-06-20 15:53:18 +0300

    Update installPostgress.sh

--- installPostgress.sh
+++ installPostgress.sh
@@ -1,5 +1,4 @@
-deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-  sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo apt-get update
-apt-get install postgresql-9.4
+sudo apt-get install postgresql postgresql-contrib
